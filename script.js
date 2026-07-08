f86e41a2094efbdcfa172076b9e3469d774f7036
nandanakrishnan0812-hash <nandanakrishnan0812@gmail.com>
2026-07-08 18:50:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const startBtn = document.getElementById("startBtn");
 
+startBtn.addEventListener("click", () => {
+    document.querySelector(".loading").style.display = "block";
+    document.querySelector(".welcome").style.display = "none";
+
+    setTimeout(() => {
+        alert("❤️ Welcome to Our 4 Years of Love ❤️");
+        // Quiz next step ivide add cheyyam
+    }, 2000);
+});
